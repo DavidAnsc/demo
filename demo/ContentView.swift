e2eda86053a217bf8c1239371bf93bf4a710cd18
David_an SEATURTLE <ansichen.david@gmail.com>
2025-04-19 18:14:35 +0800

    My second commit

--- demo/ContentView.swift
+++ demo/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Fuck you bro!!")
+			Text("I love u!!")
         }
         .padding()
     }
