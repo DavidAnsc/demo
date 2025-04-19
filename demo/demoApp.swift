d3b259c50affbbf418a2890ffafd99bf0c037b7b
David_an SEATURTLE <ansichen.david@gmail.com>
2025-04-19 17:57:55 +0800

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by David An on 19/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
