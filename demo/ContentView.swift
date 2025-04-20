40e91e5efbdbcd23f6314b862214969461b98c73
David An <ansichen.david@gmail.com>
2025-04-20 10:24:58 +0800

    my xxx commit

--- demo/ContentView.swift
+++ demo/ContentView.swift
@@ -10,15 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-			let value = "Hello guys, I am invisible."
-			Text(value)
-			
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Fuck you bro!!")
-			Text("I love u!!")
-			Text("That girl is sexy!!")
         }
         .padding()
     }
