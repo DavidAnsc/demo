476e822872f93805f927d250c7193e7e8bb0ab43
David An <ansichen.david@gmail.com>
2025-04-20 10:53:44 +0800

    [clean] Add detailed document

--- demo/demoApp.swift
+++ demo/demoApp.swift
@@ -5,6 +5,17 @@
 //  Created by David An on 19/04/2025.
 //
 
+/*
+
+This is an app that I made since Apr 20, 2025. This is my source control course demo app to help me learn.
+
+
+
+
+
+
+*/
+
 import SwiftUI
 
 @main
