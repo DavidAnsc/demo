bf73b9d8309cdfcafe5801ec2e35dbf319601e2e
David An <ansichen.david@gmail.com>
2025-04-26 18:53:35 +0800

    [View] Add ExplorerView

---
+++ demo/ExplorerView.swift
@@ -0,0 +1,19 @@
+//
+//  ExplorerView.swift
+//  demo
+//
+//  Created by David An on 26/04/2025.
+//
+
+import SwiftUI
+var myName = "David"
+
+struct ExplorerView: View {
+    var body: some View {
+        Text("hello, my name is \(myName)!")
+    }
+}
+
+#Preview {
+    ExplorerView()
+}
