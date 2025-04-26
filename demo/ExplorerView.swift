4768fe77001f42a9af56d8335f7d7d292e52ceff
David An <ansichen.david@gmail.com>
2025-04-26 19:27:57 +0800

    [feature] add sentences in ExplorerView

--- demo/ExplorerView.swift
+++ demo/ExplorerView.swift
@@ -11,6 +11,8 @@ var myName = "David"
 struct ExplorerView: View {
     var body: some View {
         Text("hello, my name is \(myName)!")
+			.padding()
+		Text("I am a video editing enthusiast, graphic design enthusiast & a coding enthusiast. What are you interest in??")
     }
 }
 
