483d6bafd6662f98f2a59c8cbc10d521197571cd
David An <ansichen.david@gmail.com>
2025-05-13 20:39:52 +0800

    [feature] Add kerning to text

--- demo/ExplorerView.swift
+++ demo/ExplorerView.swift
@@ -11,7 +11,7 @@ var myName = "David"
 struct ExplorerView: View {
     var body: some View {
         Text("hello, my name is \(myName)!")
-			.padding()
+			.kerning(5)
 		Text("I am a video editing enthusiast, graphic design enthusiast & a coding enthusiast. What are you interest in??")
 		RoundedRectangle(cornerRadius: 30)
 			.frame(width: 70, height: 100)
